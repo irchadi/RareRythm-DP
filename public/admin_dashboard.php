1cfb7a75cb7643a5a789916282e0cb530ccbdf63
irchadi <84560636+irchadi@users.noreply.github.com>
2024-04-26 00:15:37 +0200

    genres redirection

--- public/admin_dashboard.php
+++ public/admin_dashboard.php
@@ -105,6 +105,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Tableau de bord administrateur - RareRythm</title>
+    <link href="css/style.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
